97ffac789042856907ab4ddb33b70feeee8d48eb
drsax <drsax@naver.com>
2016-08-18 17:43:34 +0900

    Add files via upload

--- mod_demo.js
+++ mod_demo.js
@@ -1,7 +1,6 @@
 
-var DAX = new AudioContext();
-   
-   
+ var DAX = new AudioContext();
+var osc =DAX.createOscillator();
 
 (function() {
 
@@ -10,7 +9,7 @@ var DAX = new AudioContext();
     var root = this;
     var version = '1.0';
     var DrSAX;
-    var osc = DAX.createOscillator();
+  
 
     if(typeof exports !== 'undefined') {
         DrSAX= exports;
@@ -20,65 +19,20 @@ var DAX = new AudioContext();
 
     }
 
+  DrSAX.abc ="dsdd"
 
 
-
-  DrSAX.get = function() {
-        return version;
+  DrSAX.get = function(a,b) {
+       var sum =  a+b
+        return sum;
     }
     
-  
+
 
 
 }());
 
 
-
-/*
-
-var clerk = (function() {
-    var name = 'Teo';
-    var sex = '남자';
-    var position = '수석 엔지니어';
-    // salary private
-    var salary = 2000;
-    var taxSalary = 200;
-    var totalBonus = 100;
-    var taxBonus = 10;
- 
-    var payBonus = function() {
-        totalBonus = totalBonus - taxBonus;
-        return totalBonus;
-    };
-    var paySalary = function() {
-        return salary - taxSalary;
-    };
- 
-    // Public 속성, 메소드
-    return {
-        name : name,
-        sex : sex,                 
-        position : position,
-        paySalary : paySalary,
-        payBonus : payBonus
-    };
-}());
- 
-// name 속성은 public
-console.log(clerk.name); // 'Teo' 출력
-// salary 변수는 즉시실행함수 내부 변수이므로 private
-console.log(clerk.salary); // undefined 출력
- 
-// paySalary 메소드는 public
-console.log(clerk.paySalary()); // 1800 출력
- 
-// payBonus 메소드는 public
-console.log(clerk.payBonus()); // 90 출력
-console.log(clerk.payBonus()); // 80 출력
-
-
-
-*/
 
 
 
