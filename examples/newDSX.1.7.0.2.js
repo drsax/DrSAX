06683ed41d0f33f77982c61f7c8b30c07178c19c
Euy Shick Hong <drsax@naver.com>
2016-11-10 16:01:24 +0900

    Add files via upload

--- examples/newDSX.1.7.0.2.js
+++ examples/newDSX.1.7.0.2.js
@@ -1893,7 +1893,85 @@ DSX.prototype.pitchShift = function(overlapRatio,pitchRatio) {
 
 
 
+//////////////////////
 
+
+        
+   DSX.prototype.Record = function(source,out,cfg) {
+
+                this.out=out
+                var config = cfg || {};
+                var bufferLen = config.bufferLen || 4096;
+                this.context = source.context;
+                this.node = (this.context.createScriptProcessor ||
+                    this.context.createJavaScriptNode).call(this.context,
+                    bufferLen, 2, 2);
+                var worker = new Worker('record.js');
+                worker.postMessage({
+                    command: 'init',
+                    config: {
+                        sampleRate: this.context.sampleRate
+                    }
+                });
+                var recording = false,
+                    currCallback;
+                this.node.onaudioprocess = function(e) {
+                    if (!recording) return;
+                    worker.postMessage({
+                        command: 'record',
+                        buffer: [
+                            e.inputBuffer.getChannelData(0),
+                            e.inputBuffer.getChannelData(1)
+                        ]
+                    });
+                }
+                this.configure = function(cfg) {
+                    for (var prop in cfg) {
+                        if (cfg.hasOwnProperty(prop)) {
+                            config[prop] = cfg[prop];
+                        }
+                    }
+                }
+                this.record = function() {
+                    recording = true;
+                }
+                this.stop = function() {
+                    recording = false;
+                }
+                this.clear = function() {
+                    worker.postMessage({
+                        command: 'clear'
+                    });
+                }
+                this.getBuffer = function(cb) {
+                    currCallback = cb || config.callback;
+                    worker.postMessage({
+                        command: 'getBuffer'
+                    })
+                }
+                this.exportWAV = function(cb, type) {
+                    currCallback = cb || config.callback;
+                    type = type || config.type || 'audio/wav';
+                    if (!currCallback) throw new Error('Callback not set');
+                    worker.postMessage({
+                        command: 'exportWAV',
+                        type: type
+                    });
+                }
+                worker.onmessage = function(e) {
+                    var blob = e.data;
+                    currCallback(blob);
+                }
+                source.connect(this.node);
+                this.node.connect(out);
+         
+
+                //this should not be necessary
+            };
+
+      
+
+          //  window.Record = Record;
 
 
 
